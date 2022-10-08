b9d5b904733f6f6264137df1bf12d78ee8fc814f
kraynor856 <54773347+kraynor856@users.noreply.github.com>
2022-10-09 00:04:10 +0100

    Add files via upload

--- sender.py
+++ sender.py
@@ -1,23 +1,25 @@
 import socket
- 
+import os
 
-# Create a client socket
+
+h_name = os.uname()
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 
- 
+
 
 # Connect to the server
 
-clientSocket.connect(("127.0.0.1",9090));
+clientSocket.connect(("192.168.1.96",9090));
 
  
 
 # Send data to server
 
 data = "hello";
-
+data2 = h_name;
 clientSocket.send(data.encode());
+clientSocket.send(data2.encode());
 
 
 
